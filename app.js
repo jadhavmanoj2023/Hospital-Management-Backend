46bef45fec4ef6f7b9fc23fca3c74ed6aa0b7e50
jadhavmanoj2023 <140938830+jadhavmanoj2023@users.noreply.github.com>
2024-07-23 00:04:46 +0530

    headers setting

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.setHeader('Access-Control-Allow-Origin', "https://hospital-mangement-frontend.vercel.app");
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
