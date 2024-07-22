a280422d75cf660ccb62fe7bf8312d0c71e0e550
jadhavmanoj2023 <140938830+jadhavmanoj2023@users.noreply.github.com>
2024-07-23 00:12:54 +0530

    headers setting

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: ["https://hospital-mangement-frontend.vercel.app", process.env.DASHBOARD_URL],
     methods: ["POST", "GET", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
