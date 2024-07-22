0ad6771ee0b4f26fe83284d0676944ea61118612
jadhavmanoj2023 <140938830+jadhavmanoj2023@users.noreply.github.com>
2024-07-22 23:52:51 +0530

    headers setting

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ config({ path: "./config/config.env" });
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
+
   next();
 });
 
